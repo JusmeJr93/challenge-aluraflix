47e028ae3ce70b4cf5846779c73ccaec7a3d893b
Junior Jusme <junior.jusme@gmail.com>
2024-07-01 17:10:14 -0600

    player position

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -6,10 +6,12 @@ import styled from "styled-components";
 const PlayerContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   padding: 20px;
   background-color: #262626;
   color: #fff;
+  min-height: 65vh;
 `;
 
 const VideoFrame = styled.iframe`
